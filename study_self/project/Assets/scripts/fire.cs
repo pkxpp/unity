2d796354f1cba448dab2b94da6a1d2f111bcaa86
page <pkxpp@126.com>
2016-01-31 23:01:13 +0800

    1-14 Instatiate(), Destroy()

--- study_self/project/Assets/scripts/fire.cs
+++ study_self/project/Assets/scripts/fire.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class fire : MonoBehaviour {
 	// 
 	int speed = 5;
+	public Transform newObject;
 
 	// Use this for initialization
 	void Start () {
@@ -15,7 +16,15 @@ public class fire : MonoBehaviour {
 		float x = Input.GetAxis ("Horizontal") * Time.deltaTime * speed;
 		float z = Input.GetAxis ("Vertical") * Time.deltaTime * speed;
 		transform.Translate(x, 0, z);
-		if (z != 0 )
-			print ("csharp");
+		//if (z != 0 )
+		//	print ("csharp");
+
+		if (Input.GetButtonDown ("Fire1")) {
+			Transform t = Instantiate(newObject, transform.position, transform.rotation) as Transform;
+
+			Vector3 dir = transform.TransformDirection(Vector3.forward);
+
+			t.rigidbody.AddForce(dir * 2800);
+		}
 	}
 }
